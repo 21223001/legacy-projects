b39381ce01c4b4d028e5749b0930c44c907e5ebd
21223001 <21223001@ed.tmu.ac.jp>
2025-03-28 13:24:26 +0900

    👍[Improvement]: VSCode macro

--- VSCode_extention/macro-replacer/src/extension.ts
+++ VSCode_extention/macro-replacer/src/extension.ts
@@ -1,26 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand('macroReplace.replaceText', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.showErrorMessage('エディタが開かれていません');
+      return;
+    }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "macro-replacer" is now active!');
+    const doc = editor.document;
+    const fullText = doc.getText();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('macro-replacer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from macro-replacer!');
-	});
+    // ここに置換ペアをベタ書き
+    const replaceList = [
+      { from: '、', to: '，' },
+      { from: '。', to: '．' },
+      { from: '（', to: '(' },
+      { from: '）', to: ')' }
+    ];
 
-	context.subscriptions.push(disposable);
+    let newText = fullText;
+    for (const pair of replaceList) {
+      newText = newText.replace(new RegExp(pair.from, 'g'), pair.to);
+    }
+
+    const edit = new vscode.WorkspaceEdit();
+    const fullRange = new vscode.Range(doc.positionAt(0), doc.positionAt(fullText.length));
+    edit.replace(doc.uri, fullRange, newText);
+    await vscode.workspace.applyEdit(edit);
+
+    vscode.window.showInformationMessage('置換（ベタ書き）を完了しました');
+  });
+
+  context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
